f40a56f1bd202789a371ddf5d8e76a39af433bf2
Narumiyarinn9987 <narumiya777rin@gmail.com>
2023-08-15 22:20:45 +0900

    Initial Commit

---
+++ Trial/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Trial
+//
+//  Created by 成瀬凛 on 2023/08/15.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Trial/TrialApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrialApp.swift
+//  Trial
+//
+//  Created by 成瀬凛 on 2023/08/15.
+//
+
+import SwiftUI
+
+@main
+struct TrialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
